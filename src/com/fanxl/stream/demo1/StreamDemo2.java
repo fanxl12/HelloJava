f8813c886e69c3da7fd4d3812d91a06da797d3e7
fanxl12 <744822514@qq.com>
2018-12-06 17:15:55 +0800

    java8 Stream流操作;

---
+++ src/com/fanxl/stream/demo1/StreamDemo2.java
@@ -0,0 +1,40 @@
+package com.fanxl.stream.demo1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * @description
+ * @author: fanxl
+ * @date: 2018/12/5 0005 9:44
+ */
+public class StreamDemo2 {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        // 从集合创建
+        list.stream();
+        list.parallelStream();
+
+        // 从数组创建
+        Arrays.stream(new int[] { 2, 3, 5 });
+
+        // 创建数字流
+        IntStream.of(1, 2, 3);
+        IntStream.rangeClosed(1, 10);
+
+        // 使用random创建一个无限流
+        new Random().ints().limit(10);
+        Random random = new Random();
+
+        // 自己产生流
+        Stream.generate(() -> random.nextInt()).limit(20);
+
+    }
+
+}
