7572f6699ac370c7db7d9254e637c776708c761d
fanxl12 <744822514@qq.com>
2019-01-23 22:37:55 +0800

    完成数组的基础封装;

--- src/com/fanxl/data/structure/array/Array.java
+++ src/com/fanxl/data/structure/array/Array.java
@@ -39,5 +39,96 @@ public class Array {
         return size == 0;
     }
 
+    public void addLast(int e) {
+        add(size, e);
+    }
 
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
+    public void add(int index, int e) {
+        if (size == data.length) {
+            throw new IllegalArgumentException("add failed. Array is full.");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("add failed. required index >=0 and index <=size.");
+        }
+        for (int i = size; i > index; i--) {
+            data[i] = data[i-1];
+        }
+        data[index] = e;
+        size ++;
+    }
+
+    public int get(int index) {
+        if (index <0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. index is illegal.");
+        }
+        return data[index];
+    }
+
+    public void set(int index, int e) {
+        if (index <0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. index is illegal.");
+        }
+        data[index] = e;
+    }
+
+    public boolean contains(int e) {
+        for (int i=0; i< size; i++) {
+            if (data[i] == e) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int remove(int index) {
+        int temp = get(index);
+        for (int i=index; i<size; i++) {
+            data[i] = data[i+1];
+        }
+        size--;
+        return temp;
+    }
+
+    public int removeFirst() {
+        return remove(0);
+    }
+
+    public int removeLast() {
+        return remove(size - 1);
+    }
+
+    public void removeElement(int e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    public int find(int e) {
+        for (int i=0; i< size; i++) {
+            if (data[i] == e) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(String.format("Array: size=%d, capacity=%d\n", size, getCapacity()));
+        stringBuffer.append("[");
+        for (int i=0; i<size; i++) {
+            stringBuffer.append(data[i]);
+            if (i != size-1) {
+                stringBuffer.append(",");
+            }
+        }
+        stringBuffer.append("]");
+        return stringBuffer.toString();
+    }
 }
---
+++ src/com/fanxl/data/structure/array/Test.java
@@ -0,0 +1,24 @@
+package com.fanxl.data.structure.array;
+
+/**
+ * @description
+ * @author: fanxl
+ * @date: 2019/1/23 0023 21:58
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Array array = new Array();
+        array.addLast(10);
+        array.addLast(1);
+        array.addLast(3);
+        array.addLast(9);
+        array.add(2, 5);
+        System.out.println(array.toString());
+        array.remove(2);
+        System.out.println(array.toString());
+        array.removeElement(9);
+        System.out.println(array.toString());
+    }
+
+}
