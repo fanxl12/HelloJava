01e9d7c206fc7c88e46df7a81543d1ee9647ce42
xlfan10 <xlfanxl10@iflytek.com>
2020-06-22 17:38:30 +0800

    提交冒泡排序;

---
+++ src/com/fanxl/algorithm/BubbleSorting.java
@@ -0,0 +1,68 @@
+package com.fanxl.algorithm;
+
+/**
+ * @author fanxl12
+ * @description 冒泡排序
+ * @date 2019/6/17 14:53
+ */
+public class BubbleSorting {
+
+    public static void main(String[] args) {
+        int[] nums = {5, 2, 3, 1};
+        int[] nums2 = {5, 4, 3, 1};
+        BubbleSorting sb = new BubbleSorting();
+        int[] result = sb.sortArray(nums2);
+        sb.getResult(result);
+    }
+
+    private void getResult(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            System.out.print(nums[i] + ", ");
+        }
+    }
+
+    public int[] sortArray(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            boolean sorted = true;
+            for (int j=0; j<nums.length-1-i; j++) {
+                if (nums[j] > nums[j+1]) {
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                    sorted = false;
+                }
+            }
+            if (sorted) {
+                break;
+            }
+            getResult(nums);
+            System.out.println("第" + (i+1) + "轮");
+        }
+        return nums;
+    }
+
+    // 冒泡排序：超时
+    public int[] sortArray2(int[] nums) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            // 先默认数组是有序的，只要发生一次交换，就必须进行下一轮比较，
+            // 如果在内层循环中，都没有执行一次交换操作，说明此时数组已经是升序数组
+            boolean sorted = true;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j, j + 1);
+                    sorted = false;
+                }
+            }
+            if (sorted) {
+                break;
+            }
+        }
+        return nums;
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+}
